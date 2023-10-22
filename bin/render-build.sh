12e7d0e77cbc46a68a766945b6d932471b0ab5a2
Don Anh Nguyen <thedonwind69@gmail.com>
2023-10-22 10:40:40 -0700

    edit render file again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,13 @@
 set -o errexit
 
 bundle install
+# clean
+rm -rf public
+# build
+npm install --force
+npm run build
+
+# migrate
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
